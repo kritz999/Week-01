84cd462416a13134d9974167da709493f227cc1f
kritz999 <kritikabdas007@gmail.com>
2025-01-12 19:19:51 +0530

    Average code push

---
+++ Day-01/Level-01/Average.java
@@ -0,0 +1,23 @@
+public class Average
+{
+  public static void main(String[] args)
+  {
+   
+   int Maths = 94;
+   int Physics =95;
+   int Chemistry =100;
+    
+   double AverageMarks = (double) (Maths + Physics + Chemistry)*100 /3;
+   
+   System.out.println(" Sam's average marks in PCM is " + AverageMarks);
+
+   }
+
+ }
+
+
+
+
+
+
+
