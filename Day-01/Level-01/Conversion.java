29e5c97b6ec9c387b2b167c312213f19314dea8f
kritz999 <kritikabdas007@gmail.com>
2025-01-12 19:24:06 +0530

    Conversion code added

---
+++ Day-01/Level-01/Conversion.java
@@ -0,0 +1,22 @@
+public class Conversion
+{
+  
+  public static void main(String[]args)
+  {
+
+   double distance = 10.8;
+   
+   double miles = distance /1.6;
+   System.out.println(" The distance "+ 10.8 + " km in miles is " + miles);
+
+
+  }
+
+}
+
+
+
+
+
+
+
