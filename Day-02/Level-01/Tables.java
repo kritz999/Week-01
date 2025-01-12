6bbfa0a3668aa0c47206477086f3452d5c443e67
kritz999 <kritikabdas007@gmail.com>
2025-01-12 23:55:09 +0530

    tables are added

---
+++ Day-02/Level-01/Tables.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Tables{
+  public static void main(String args[])
+{
+  	 Scanner input= new Scanner(System.in);
+  	 int number;
+   	System.out.println("enter number between 6 and 9: ");
+   	number= input.nextInt();
+
+   	if(number>=6 &&  number<=9){
+      	for(int i=1; i<=10; i++){
+        System.out.println(number+"X"+i+" = "+number*i);
+       }
+    }
+  }
+}
+   
