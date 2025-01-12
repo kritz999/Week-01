6bf111adcabe6ed56180ec45683ebc3e130fd244
kritz999 <kritikabdas007@gmail.com>
2025-01-12 19:32:59 +0530

    pen is added

---
+++ Day-01/Level-01/Pen.java
@@ -0,0 +1,18 @@
+public class Pen
+{
+
+  public static void main(String [] args)
+  {
+
+  int pens = 14;
+  int students = 3;
+   
+  int pensPerStudent =  pens/students;
+
+  int pensRemaining =  pens% students;
+
+  System.out.println("The Pen Per Student is " + pensPerStudent + "  and the remaining pen not distributed is " + pensRemaining );
+
+   }
+
+ }
